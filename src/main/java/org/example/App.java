e705b427b9aada296fd8a45f7f2c39ac7ed601aa
catalin1989 <catalin_chira05@yahoo.com>
2023-05-02 09:26:46 +0300

    Am creat un program care salveaza un text intr-un fisier.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.BitSet;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scrieri f= new Scrieri();
+        f.setSize(200,200);
+        f.interfataGrafica();
+        f.setVisible(true);
+
+        }
+    }
+
+
