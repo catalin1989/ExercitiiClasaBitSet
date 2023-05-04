29cac703814df063e07aef7fc19b3b6b9b856e2a
catalin1989 <catalin_chira05@yahoo.com>
2023-05-04 10:06:13 +0300

    Am creat un program care cauta nume intr-un fisier txt.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,12 +6,15 @@ import java.sql.SQLOutput;
 import java.util.BitSet;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scrieri f= new Scrieri();
-        f.setSize(200,200);
+        CautareFisier f= new CautareFisier();
+        f.setBounds(200,100,400,150);
         f.interfataGrafica();
+        f.setTitle("Cautare in fisier");
         f.setVisible(true);
+
+
 
         }
     }
